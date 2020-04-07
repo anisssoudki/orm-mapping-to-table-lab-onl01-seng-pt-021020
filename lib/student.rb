5dd58b35321adc5ce9b6793dbbe2505fadeb41e2
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-07 18:55:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,33 +1,18 @@
 class Student
 
-  attr_accessor :name, :grade
+ attr_accessor :name, :grade
   attr_reader :id
- 
-  def initialize(name, grade, id=nil)
-   
+
+  def initialize(name, grade)
     @name = name
     @grade = grade
   end
- 
+
   def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE Students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
-        )
-        SQL
-    DB[:conn].execute(sql) 
-  end
- 
-  def save
-    sql = <<-SQL
-      INSERT INTO Students (name, grade) 
-      VALUES (?, ?)
+    sql = <<-SQL 
+    CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER);
     SQL
- 
-    DB[:conn].execute(sql, self.name, self.grade)
- 
+      DB[:conn].execute(sql)
   end
  
 
